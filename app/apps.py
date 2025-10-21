e5e34b0501fe432b273d610f438dc5ec611e0306
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-10-21 10:32:56 -0300

    Se agregó el signals.py para crear usuario invitado al momento de crear la bd

--- app/apps.py
+++ app/apps.py
@@ -5,10 +5,5 @@ class AppConfig(AppConfig):
     name = 'app'
 
     def ready(self):
-        from django.contrib.auth.models import User
-        # Crear usuario invitado si no existe
-        if not User.objects.filter(username='invitado').exists():
-            User.objects.create_user(
-                username='invitado',
-                password='pass_invitado'
-            )
+        # Importa la función que crea el usuario en signals.py
+        import app.signals
