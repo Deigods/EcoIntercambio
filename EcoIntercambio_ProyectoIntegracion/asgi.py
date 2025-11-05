e3c4c295063085355b5787b0a986028306f9162e
Diego González <diego.gonzalez217@inacapmail.cl>
2025-11-05 05:17:34 -0300

    Fixed database and staticfiles, test 12

--- EcoIntercambio_ProyectoIntegracion/asgi.py
+++ EcoIntercambio_ProyectoIntegracion/asgi.py
@@ -1,25 +1,20 @@
 """
 ASGI config for EcoIntercambio_ProyectoIntegracion project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
-
-# ⚙️ Configura Django primero
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoIntercambio_ProyectoIntegracion.settings')
-
-# Inicializa Django antes de importar Channels o tus apps
-django_asgi_app = get_asgi_application()
-
-# Solo después de eso importa Channels y routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import app.routing
+
+# ⚙️ Configura Django antes de importar Channels
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoIntercambio_ProyectoIntegracion.settings')
+django.setup()
+
+# Inicializa Django
+django_asgi_app = get_asgi_application()
 
 # 🚀 Aplica configuración final
 application = ProtocolTypeRouter({
